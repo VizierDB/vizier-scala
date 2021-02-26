b23ee129bc9e22b5d3bac3e4e777b035cec849cf
Oliver <okennedy@buffalo.edu>
2021-02-26 15:11:47 -0500

    missed a file

--- src/main/scala/info/vizierdb/Config.scala
+++ src/main/scala/info/vizierdb/Config.scala
@@ -24,19 +24,27 @@ class Config(arguments: Seq[String])
   extends ScallopConf(arguments)
   with LazyLogging
 {
-  version("Vizier-Scala 0.2 (c) 2020 Univ. at Buffalo, New York Univ., and Illinois Inst. of Tech.")
-  banner("""Usage: vizier [OPTIONS]
+  version("Vizier-Scala 0.5 (c) 2020 U. Buffalo, NYU, Ill. Inst. Tech., and Breadcrumb Analytics")
+  banner("""Docs: https://github.com/VizierDB/web-ui/wiki
+           |Usage: vizier [OPTIONS]
            |    or vizier import [OPTIONS] export
            |""".stripMargin)
   val defaults = Config.loadDefaults()
 
+  val help = opt[Boolean]("help",
+    descr = "Display this help message",
+    default = Option(false)
+  )
+
   val googleAPIKey = opt[String]("google-api-key", 
     descr = "Your Google API Key (for Geocoding)",
-    default = Option(defaults.getProperty("google-api-key"))
+    default = Option(defaults.getProperty("google-api-key")),
+    noshort = true
   )
   val osmServer = opt[String]("osm-server",
     descr = "Your Open Street Maps server (for Geocoding)",
-    default = Option(defaults.getProperty("osm-server"))
+    default = Option(defaults.getProperty("osm-server")),
+    noshort = true
   )
   val basePath = opt[File]("database",
     descr = "Path to the project database (e.g., vizier.db)",
@@ -58,7 +66,8 @@ class Config(arguments: Seq[String])
     descr = "The Public-Facing URL of Vizier (e.g., for use with proxies)",
     default = Option(defaults.getProperty("vizier-public-url"))
   )
-  val experimental = opt[List[String]]("X", 
+  val experimental = opt[List[String]]("experiment", 
+    short = 'X',
     descr = "Enable an experimental option",
     default = Some(List[String]()))
 
@@ -67,6 +76,12 @@ class Config(arguments: Seq[String])
     default = Option(defaults.getProperty("vizier-developer-mode"))
                 .map { _.toLowerCase.equals("true") }
                 .orElse { Some(false) }
+  )
+
+  val connectFromAnyHost = opt[Boolean]("connect-from-any-host",
+    descr = "Allow connections from any IP (WARNING: this will let anyone on your network run code on your machine)",
+    default = Option(false),
+    noshort = true
   )
 
   object ingest extends Subcommand("import", "ingest") {
