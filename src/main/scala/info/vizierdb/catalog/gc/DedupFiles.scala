cf09c589de221129530c8cd2b34c18f34cea0ee4
Oliver <okennedy@buffalo.edu>
2021-09-24 17:19:54 -0400

    First steps towards a garbage collector: File deduplication (WIP) (re: #142)
    - Added a new garbage collector subcommand (not presently used
    - MutableProject now optionallyallows a UI-like file upload during the load step
    - Can now get all artifacts in one call
    - Added a new method to Parameter and Command to allow map-like parameter rewriting
    - info.vizierdb.catalog.gc.DedupFiles is a WIP to add support for deduplicating files.
    - Added a test case for DedupFiles
    
    The key thing that still needs to happen at this point is propagating de-duplicated files into Mimir; The duplicate files get deleted, so any references to them in Mimir need to be replaced accordingly.

---
+++ src/main/scala/info/vizierdb/catalog/gc/DedupFiles.scala
@@ -0,0 +1,182 @@
+package info.vizierdb.catalog.gc
+
+import java.io.File
+import scala.io.Source
+import scala.util.hashing.MurmurHash3
+import scalikejdbc._
+import info.vizierdb.types._
+import info.vizierdb.catalog.Artifact
+import com.typesafe.scalalogging.LazyLogging
+import org.mimirdb.util.TimerUtils
+import scala.collection.mutable
+import info.vizierdb.catalog.{
+  Module,
+  ArtifactRef,
+  InputArtifactRef,
+  OutputArtifactRef
+}
+import play.api.libs.json._
+import info.vizierdb.commands.Command
+import info.vizierdb.commands.Parameter
+import info.vizierdb.commands.FileArgument
+import info.vizierdb.commands.FileParameter
+
+object DedupFiles 
+  extends LazyLogging
+{
+  def apply() = dedup(None)
+  def apply(projectId: Identifier) = dedup(Some(projectId))
+
+  def getFileHash(file: File): Int =
+  {
+    MurmurHash3.orderedHash(
+      Source.fromFile(file).getLines()
+    )
+  }
+
+  def filesAreIdentical(file1: File, file2: File): Boolean = 
+  {
+    TimerUtils.logTime(s"Compare $file1 vs $file2", log = logger.trace(_)) {
+      val data1 = Source.fromFile(file1)
+      val data2 = Source.fromFile(file2)
+
+      while( data1.hasNext && data2.hasNext )
+      {
+        if(data1.next() != data2.next()) { return false }
+      }
+      if(data1.hasNext){ return false } // len(file1) > len(file2)
+      if(data2.hasNext){ return false } // len(file1) < len(file2)
+      return true
+    }
+  }
+
+  def fixModuleFileArguments(
+    canonicalFileId: Identifier,
+    fileIdsToReplace: Set[Identifier],
+    command: Command,
+    arguments: JsObject
+  ): Option[JsObject] =
+  {
+    command.replaceArguments(arguments){
+      case (_:FileParameter, arg: JsObject)
+        if(fileIdsToReplace.intersect(arg.as[FileArgument].fileid.toSet).size > 0)
+          => Json.toJson(
+               arg.as[FileArgument].copy(fileid = Some(canonicalFileId))
+             )
+    }.map { _.as[JsObject] }
+  }
+  
+  def coalesceFileArtifacts(artifacts: Seq[Artifact])
+  {
+    logger.debug(s"Coalescing : ${artifacts.map { _.id }.mkString(", ")}")
+    if(artifacts.size <= 1){ return; }
+
+    // Arbitrarily select the file with the lowest id to preserve
+    val sorted = artifacts.sortBy { _.id }
+    val canonicalArtifactId = sorted.head.id
+
+    // Coalesce the rest, replacing them with [[target]]
+    val artifactsToRename = sorted.tail
+    val artifactIdsToRename = artifactsToRename.map { _.id }
+
+    DB.autoCommit { implicit s => 
+      // File artifacts arise in the following contexts: 
+      //   InputArtifactRef
+      //   OutputArtifactRef
+      //   FileArgument (in modules)
+      withSQL { 
+        val a = OutputArtifactRef.column
+        update(OutputArtifactRef)
+          .set(
+            a.artifactId -> canonicalArtifactId
+          ).where.in(
+            a.artifactId, artifactIdsToRename
+          ) 
+      }.update.apply()
+      withSQL { 
+        val a = InputArtifactRef.column
+        update(InputArtifactRef)
+          .set(
+            a.artifactId -> canonicalArtifactId
+          ).where.in(
+            a.artifactId, artifactIdsToRename
+          ) 
+      }.update.apply()
+
+      val allModules = 
+        withSQL {
+          val m = Module.syntax
+          select
+            .from(Module as m)
+        }.map { Module(_) }.list.apply()
+
+      for(m <- allModules){
+        fixModuleFileArguments(
+          canonicalArtifactId, 
+          artifactIdsToRename.toSet,
+          m.command.get, 
+          m.arguments
+        ) match { 
+          case None => ()
+          case Some(newArgs) => {
+            logger.trace(s"Updating File ID for $m")
+            m.replaceArguments(newArgs)
+          }
+        }
+      }
+    }
+    for(file <- artifactsToRename){
+      file.absoluteFile.delete()
+    }
+  }
+
+  def dedup(projectId: Option[Identifier] = None): Unit = 
+  {
+    val files = 
+      DB.readOnly { implicit s =>
+        Artifact.all(projectId)
+                .filter { _.t == ArtifactType.FILE }
+                .toSeq
+      }
+
+    logger.debug(s"Checking ${files.size} files for duplicates")
+
+    val filesGroupedByHash = 
+      TimerUtils.logTime("Compute File Hashes", log = logger.debug(_)) 
+      { 
+        files.groupBy { f => getFileHash(f.absoluteFile) }
+      }
+
+    for( (hash, candidateFileGroup) <- filesGroupedByHash )
+    {
+      logger.trace(s"Processing Hash $hash:\n${
+        candidateFileGroup.map { "---> "+_.relativeFile }.mkString("\n")
+      }")
+
+      // Skip any file (group) that's already unique
+      if(candidateFileGroup.size > 1){
+
+        // Subgroup [[candidateFileGroup]] by checking for file equivalence.  In general, this 
+        // should only ever produce one group (unless we have a hash collision)
+
+        val identicalFileGroups = new mutable.ArrayBuffer[mutable.ArrayBuffer[Artifact]]()
+        for(file <- candidateFileGroup){
+          val idx = identicalFileGroups.indexWhere{ cmp => 
+              filesAreIdentical(cmp.head.absoluteFile, file.absoluteFile)
+            }
+          if(idx >= 0){
+            identicalFileGroups(idx).append(file)
+          } else {
+            identicalFileGroups.append(mutable.ArrayBuffer(file))
+          }
+        }
+
+        for( fileGroup <- identicalFileGroups ){
+          if(fileGroup.size > 1) { coalesceFileArtifacts(fileGroup) }
+        }
+      }
+    }
+
+  }
+
+}
