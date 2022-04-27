VizierDB/vizier-scala

commit c19599543882ff3c25d55063d5f7455913352a90

Adding Scala snippets (re: #168)
- Adding Scala snippets
- Cleaning up ExecutionContext API a bit.
   - datasetPipeline is now getPipeline
   - Added `file(name)` to access a file artifact
   - Parameter is now typed, and returns a value, rather than an option
   - outputDataframe and createPipeline now use the outputDatasetWithFile operation

vizier/backend-src/info/vizierdb/commands/ExecutionContext.scala
@@ -45,6 +45,7 @@ import org.apache.spark.ml.Pipeline
 import info.vizierdb.spark.PipelineModelConstructor
 import info.vizierdb.spark.LoadConstructor
 import org.apache.spark.ml.PipelineModel
+import scala.io.Source
 
 class ExecutionContext(
   val projectId: Identifier,
@@ -149,7 +150,7 @@ class ExecutionContext(
   /**
    * Define and fit SparkML Pipeline
    */
-  def pipeline(
+  def createPipeline(
     input: String, 
     output: String = null,
     properties: Map[String,JsObject] = Map.empty
@@ -179,73 +180,36 @@ class ExecutionContext(
     logger.debug("Fitting pipeline")
     val model = pipeline.fit(inputDataframe)
 
-    val outputArtifact = 
-      DB.autoCommit { implicit s =>
-
-        logger.debug("Creating pipeline placeholder artifact")
-        // Create a placeholder
-        var artifact = Artifact.make(
-          projectId,
-          ArtifactType.DATASET,
-          MIME.RAW,
-          Array()
-        )
-
-        logger.debug(s"Saving pipeline model to ${artifact.absoluteFile}")
-        // Output the model
-        model.save(artifact.absoluteFile.toString)
-
-        // Fill out the placeholder
-        artifact = artifact.replaceData(
-          Json.toJson(Dataset(
-            new PipelineModelConstructor(
-              input = inputArtifact.id,
-              url = FileArgument(fileid = Some(artifact.id)),
-              projectId = projectId
-            ),
-            properties = properties
-          ))
-        )
-
-        /* return */ artifact
-      }
-
-    this.output(Option(output).getOrElse(input), outputArtifact)
+    val outputArtifact = outputDatasetWithFile(
+        Option(output).getOrElse(input), 
+        { artifact => 
+          new PipelineModelConstructor(
+            input = inputArtifact.id,
+            url = FileArgument(fileid = Some(artifact.id)),
+            projectId = projectId
+          )
+        },
+        properties
+    )
 
     return (outputArtifact, model)
   }
 
   def outputDataframe(name: String, dataframe: DataFrame, properties: Map[String,JsObject] = Map.empty): Artifact =
   {
-    val artifact =
-      DB.autoCommit { implicit s =>
-        // Create a placeholder
-        val artifact = Artifact.make(
-          projectId,
-          ArtifactType.DATASET,
-          MIME.RAW,
-          Array()
-        )
+    outputDatasetWithFile(name, { artifact =>
+      dataframe.write
+               .parquet(artifact.absoluteFile.toString)
 
-        artifact.replaceData(
-          Json.toJson(Dataset(
-            new LoadConstructor(
-              url = FileArgument(fileid = Some(artifact.id)),
-              format = "parquet",
-              sparkOptions = Map(),
-              contextText = Some(name),
-              proposedSchema = Some(dataframe.schema),
-              projectId = projectId
-            ),
-            properties = properties
-          ))
-        )
-      }
-
-    dataframe.write
-             .parquet(artifact.absoluteFile.toString)
-
-    return artifact
+      new LoadConstructor(
+        url = FileArgument(fileid = Some(artifact.id)),
+        format = "parquet",
+        sparkOptions = Map(),
+        contextText = Some(name),
+        proposedSchema = Some(dataframe.schema),
+        projectId = projectId
+      )
+    }, properties)
   }
 
   /** 
@@ -286,12 +250,22 @@ class ExecutionContext(
    *
    * Parameters should be valid spark data values of the type provided.
    */
-  def parameter(name: String): Option[serialized.ParameterArtifact] =
+  def parameterOpt[T](name: String): Option[T] =
   {
     artifact(name)
       .filter { _.t == ArtifactType.PARAMETER }
-      .map { _.parameter }
+      .map { _.parameter.asInstanceOf[T] }
   }
+
+  /**
+   * Get a parameter artifact defined in an earlier cell
+   * 
+   * @param   name         The name of the artifact
+   *
+   * Parameters should be valid spark data values of the type provided.
+   */
+  def parameter[T](name: String): T =
+    parameterOpt[T](name).get
 
   /**
    * Set a parameter artifact for use in later cells
@@ -400,7 +374,7 @@ class ExecutionContext(
 
   def outputDatasetWithFile[T <: DataFrameConstructor](
     name: String,
-    constructor: Identifier => T,
+    constructor: Artifact => T,
     properties: Map[String, JsValue] = Map.empty
   )(implicit writes: Writes[T]): Artifact =
   {
@@ -415,7 +389,7 @@ class ExecutionContext(
         name,
         artifact.replaceData(
           Json.toJson(Dataset(
-            constructor(artifact.id),
+            constructor(artifact),
             properties
           )).toString.getBytes
         )
@@ -457,6 +431,21 @@ class ExecutionContext(
       fout.close()
     }
     return placeholder
+  }
+
+  /**
+   * Retrieve a file artifact
+   */
+  def file[T](name: String)(readFile: Source => T): T =
+  {
+    val fileArtifact = artifact(name).getOrElse { throw new VizierException(s"File $name does not exist")}
+    val data = fileArtifact.t match {
+                  case ArtifactType.FILE => Source.fromFile(fileArtifact.absoluteFile)
+                  case ArtifactType.BLOB => Source.fromBytes(fileArtifact.data)
+               }
+    val ret = readFile(data)
+    data.close()
+    return ret
   }
 
   /**
@@ -550,7 +539,7 @@ class ExecutionContext(
       }
     }
 
-  def datasetPipeline(name: String): Option[PipelineModel] =
+  def pipeline(name: String): Option[PipelineModel] =
     dataset(name).flatMap { 
       _.constructor match {
         case p:PipelineModelConstructor => Some(p.pipeline)

vizier/backend-src/info/vizierdb/commands/data/CheckpointDataset.scala
@@ -64,11 +64,11 @@ object CheckpointDataset extends Command
 
     context.outputDatasetWithFile(
       datasetName,
-      artifactId => 
+      artifact => 
         new MaterializeConstructor(
           input = input.id,
           schema = df.schema,
-          artifactId = artifactId,
+          artifactId = artifact.id,
           projectId = context.projectId,
           format = MaterializeConstructor.DEFAULT_FORMAT,
           options = Map.empty

vizier/ui/src/info/vizierdb/ui/components/parameter.scala
@@ -15,6 +15,7 @@ import info.vizierdb.serialized
 import info.vizierdb.serializers._
 import info.vizierdb.ui.components.snippets.PythonSnippets
 import info.vizierdb.ui.components.snippets.SnippetsBase
+import info.vizierdb.ui.components.snippets.ScalaSnippets
 
 class ParameterError(msg: String, val parameter: Parameter) extends Exception(msg)
 
@@ -284,7 +285,7 @@ case class CodeParameter(
         }
       ),
       CodeParameter.SNIPPETS.get(language).map { _.apply { snippet => 
-        editor.replaceSelection(snippet)
+        editor.replaceSelection("\n"+snippet)
       }}
     )
   def value = 
@@ -311,7 +312,8 @@ object CodeParameter
   )
 
   val SNIPPETS = Map[String, SnippetsBase](
-    "python" -> PythonSnippets
+    "python" -> PythonSnippets,
+    "scala" -> ScalaSnippets
   )
 }
 

vizier/ui/src/info/vizierdb/ui/components/snippets/ScalaSnippets.scala
@@ -1,0 +1,84 @@
+package info.vizierdb.ui.components.snippets
+
+
+object ScalaSnippets extends SnippetsBase
+{
+  AddGroup("desktop", "Read")(
+    "Get Spark Dataframe" -> 
+      """// This returns a read-only spark dataframe
+        |val df = vizierdb.dataframe("DATASET_NAME")""".stripMargin,
+
+    "Get Parameter" ->
+      """// This retrieves a string parameter.  Change the type as needed
+        |val param = vizierdb.parameter[String]("PARAMETER_NAME")""".stripMargin,
+
+    "Read File" ->
+      """// This retrieves the contents of a file as a [scala.io.Source].
+        |vizierdb.file("FILE_ARTIFACT_NAME") { source: scala.io.Source =>
+        |  // The source will be automatically closed at the end of the block
+        |}""".stripMargin,
+
+    "Get SparkML Pipeline" ->
+      """// Retrieve the ScalaML pipeline used to create the specified
+        |// dataset.  Note that this will only work if the dataset was
+        |// created with the [[vizierdb.pipeline]] method.
+        |val pipeline = vizierdb.pipeline("PIPELINE_NAME")""".stripMargin,
+  )
+
+  AddGroup("plus", "Create")(
+    "Save Spark Dataframe" ->
+      """// This saves the spark dataframe in parquet format and makes it
+        |// available to subsequent cells.  Note that, at present, caveats
+        |// will not be propagated.
+        |vizierdb.outputDataframe("DATASET_NAME", df)""".stripMargin,
+
+    "Set Parameter" ->
+      """//The provided parameter is saved.  The type is any Spark datatype
+        |vizierdb.setParameter("PARAMETER_NAME", "VALUE", StringType)""".stripMargin,
+
+    "Output File" ->
+      """//Write out a file artifact
+        |vizierdb.outputFile(
+        |  name, 
+        |  /* mimeType = "text/plain" */
+        |) { f: java.io.OutputStream =>
+        |  // write the file contents to `f`
+        |}""".stripMargin,
+  )
+
+  AddGroup("edit", "Update")(
+    "Apply SparkML Pipeline" ->
+      """// Build and apply a SparkML pipeline to the specified dataframe
+        |vizierdb.createPipeline("DATASET_NAME", /* OPTIONAL_OUTPUT_NAME */)(
+        |  // A list of SparkML Transformers, separated by commas
+        |)""".stripMargin,
+
+    "Delete Artifact" ->
+      """// Delete an artifact
+        |vizierdb.delete("DATASET_NAME")""".stripMargin,
+  )
+
+  AddGroup("comment-o", "Message")(
+    "Error Message" -> 
+      """vizierdb.error("THE_MESSAGE")""".stripMargin,
+
+    "HTML Message" ->
+      """vizierdb.displayHTML(
+        |  "THE_HTML_TEXT",
+        |  // optionally provide javascript to run when the
+        |  // message is displayed
+        |  /* javascript = "", */
+        |)""".stripMargin,
+
+    "Display Dataset" ->
+      """vizierdb.displayDataset(
+        |  "DATASET_NAME", 
+        |  // Optionally start the display at a specified offset
+        |  /* offset = 0l, */
+        |  /* limit = 100, */
+        |)""".stripMargin,
+  )
+
+}
+
+

commit 5f227a6ce9ce58d0a6220be145a58086da6f7188

Fixing recent bugs w/ Parameter and Output artifact
c19599543882ff3c25d55063d5f7455913352a90 introduced two bugs.  Fixed.

vizier/backend-src/info/vizierdb/commands/ExecutionContext.scala
@@ -183,6 +183,10 @@ class ExecutionContext(
     val outputArtifact = outputDatasetWithFile(
         Option(output).getOrElse(input), 
         { artifact => 
+          logger.debug(s"Saving pipeline model to ${artifact.absoluteFile}")
+          // Output the model
+          model.save(artifact.absoluteFile.toString)
+
           new PipelineModelConstructor(
             input = inputArtifact.id,
             url = FileArgument(fileid = Some(artifact.id)),
@@ -254,7 +258,7 @@ class ExecutionContext(
   {
     artifact(name)
       .filter { _.t == ArtifactType.PARAMETER }
-      .map { _.parameter.asInstanceOf[T] }
+      .map { _.parameter.nativeValue.asInstanceOf[T] }
   }
 
   /**