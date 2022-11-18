e6ee30208dea640da707f4b48470722b0269b813
Oliver <okennedy@buffalo.edu>
2022-11-18 10:28:20 -0500

    WIP to move python settings over to a reactive list

--- vizier/ui/src/info/vizierdb/ui/components/settings/PythonSettings.scala
+++ vizier/ui/src/info/vizierdb/ui/components/settings/PythonSettings.scala
@@ -15,120 +15,140 @@ import info.vizierdb.ui.widgets.Spinner
 import info.vizierdb.ui.widgets.Expander
 import info.vizierdb.serialized
 import info.vizierdb.ui.widgets.ShowModal
+import info.vizierdb.ui.rxExtras.RxBuffer
+import info.vizierdb.ui.rxExtras.implicits._
+import info.vizierdb.ui.rxExtras.RxBufferView
+
 
 class PythonSettings(parent: SettingsView)(implicit owner: Ctx.Owner) extends SettingsTab
 {
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
-  var environments = div(Spinner(size = 30)).render
+  sealed trait PythonPackageEntry { def root: dom.html.Element }
+
+  case class PythonPackage(pkg: serialized.PythonPackage) extends PythonPackageEntry
+  {
+    val version = Var[Option[dom.html.Input]](None)
+
+    val root =
+      tr(
+        td(
+          a(
+            href := s"https://pypi.org/project/${pkg.name}/",
+            target := "_blank",
+            pkg.name
+          )
+        ),
+        td(
+          version.map {
+            _.getOrElse { span(pkg.version).render }
+          }.reactive
+        ),
+        td(`class` := "actions",
+          a(`class` := "fabutton",
+            FontAwesome("trash"), href := "#", 
+            onclick := { _:dom.Event => println(s"remove $name") }),
+          a(`class` := "fabutton",
+            FontAwesome("level-up"), href := "#", 
+            onclick := { _:dom.Event => println(s"upgrade $name") }),
+        )
+      ).render
+  }
+
+  case class NewPackage() extends PythonPackageEntry
+  {
+    val name = input(`type` := "text").render
+    val version = input(`type` := "text").render
+
+    val root = 
+      tr(`class` := "tentative_package",
+        td(name),
+        td(version),
+        td(`class` := "actions",
+          a(`class` := "fabutton",
+            FontAwesome("trash"), href := "#", 
+            onclick := { _:dom.Event => println(s"remove $name") }),
+        )        
+      ).render
+  }
+
+  case class PythonEnvironmentEntry(name: String, version: String)
+  {
+    val packages = 
+      RxBuffer[PythonPackageEntry]()
+
+    val list_id = s"packages_for_$name"
+
+    val packagesView = 
+      RxBufferView(tbody().render, packages.rxMap { _.root })
+
+    val root = 
+      div(`class` := "environment",
+
+        // Title and Summary in the First row 
+        div(`class` := "summary",
+          Expander(list_id),               // The 'arrow' to expand the package list
+          span(`class` := "label", name),
+          span(`class` := "details", 
+            " (",
+            version,
+            ")"
+          ),
+          span(`class` := "spacer"),
+
+          // Action items in the first row
+          a(`class` := "fabutton",
+            FontAwesome("files-o"), href := "#", 
+            onclick := { _:dom.Event => println("duplicate") }),
+          a(`class` := "fabutton",
+            FontAwesome("share-square-o"), href := "#", 
+            onclick := { _:dom.Event => println("export") }),
+        ),
+
+        // Follow-up package list
+        div(`class` := "packages closed",
+          id := list_id,
+          table(
+            thead(
+              tr(
+                th("Package"),
+                th("Version"),
+                th("Actions")
+              )
+            ),
+            packagesView.root
+          ),
+          div(
+            `class` := "addpackage",
+            button(
+              FontAwesome("plus-circle"),
+              onclick := { _:dom.Event => println("install") },
+            )
+          )
+        )
+      ).render
+  }
+
+  val environments = 
+    RxBuffer[PythonEnvironmentEntry]()
+
+  val environmentsView = 
+    RxBufferView(div().render, environments.rxMap { _.root })
 
   def load(): Unit = 
   {
     Vizier.api.configListPythonEnvs()
               .onComplete { 
                 case Success(newEnvs) => 
-                  environments.innerHTML = ""
-                  for((env, summary) <- newEnvs){
-                    environments.appendChild(Environment(env, summary).render)
-                  }
-                case Failure(err) => Vizier.error(err.getMessage())
+                  environments.clear()
+                  environments.insertAll(0, 
+                    newEnvs.map { case (env, summary) =>
+                      PythonEnvironmentEntry(env, summary.pythonVersion)
+                    }
+                  )
+                case Failure(err) => 
+                  Vizier.error(err.getMessage())
               }
-  }
-
-  def Environment(name: String, summary: serialized.PythonEnvironmentSummary): dom.html.Element =
-  {
-    val details = Seq(
-      "Python "+summary.pythonVersion
-    ).mkString("; ")
-
-    val descriptor = 
-      Vizier.api.configGetPythonEnv(name)
-
-    val list_id = s"packages_for_$name"
-
-    div(`class` := "environment",
-
-      // Title and Summary in the First row 
-      div(`class` := "summary",
-        Expander(list_id),               // The 'arrow' to expand the package list
-        span(`class` := "label", name),
-        span(`class` := "details", 
-          " (",
-          details,
-          ")"
-        ),
-        span(`class` := "spacer"),
-
-        // Action items in the first row
-        a(`class` := "fabutton",
-          FontAwesome("files-o"), href := "#", 
-          onclick := { _:dom.Event => println("duplicate") }),
-        a(`class` := "fabutton",
-          FontAwesome("share-square-o"), href := "#", 
-          onclick := { _:dom.Event => println("export") }),
-      ),
-
-      // Follow-up package list
-      div(`class` := "packages closed",
-        id := list_id,
-
-        // Display the Future in a lazy-loading div.  It'll likely be loaded by 
-        // the time the user clicks the arrow button, but let's be sure.
-        Spinner.lazyLoad {
-          descriptor.map { 
-            case serialized.PythonEnvironmentDescriptor(
-              pythonVersion,
-              revision,
-              packages
-            ) =>
-            table(
-              thead(
-                tr(
-                  th("Package"),
-                  th("Version"),
-                  th("Actions")
-                )
-              ),
-              tbody(
-
-                // One row per package
-                packages.map { case serialized.PythonPackage(name, version) =>
-                  tr(
-                    td(
-                      a(
-                        href := s"https://pypi.org/project/$name/",
-                        target := "_blank",
-                        name
-                      )
-                    ),
-                    td(version),
-                    td(`class` := "actions",
-                      a(`class` := "fabutton",
-                        FontAwesome("trash"), href := "#", 
-                        onclick := { _:dom.Event => println(s"remove $name") }),
-                      a(`class` := "fabutton",
-                        FontAwesome("level-up"), href := "#", 
-                        onclick := { _:dom.Event => println(s"upgrade $name") }),
-                    )
-                  )
-                } :+
-                // And follow it with an "add package button" row
-                tr(
-                  td(
-                    `class` := "addpackage",
-                    colspan := 3,
-                    button(
-                      FontAwesome("plus-circle"),
-                      onclick := { _:dom.Event => println("install") },
-                    )
-                  )
-                )
-              )
-            ).render
-          }
-        }
-      )
-    ).render
   }
 
   def newEnvironment(): Unit =
@@ -151,7 +171,7 @@ class PythonSettings(parent: SettingsView)(implicit owner: Ctx.Owner) extends Se
   val root = div(`class` := "python",
     div(`class` := "group",
       div(`class` := "title", "Environments"),
-      environments,
+      environmentsView.root,
       div(`class` := "new_environment"),
       button(
         "New Environment",
