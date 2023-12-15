a86bef6fb5469709044af6264c4984d3cf5e9ef8
Oliver Kennedy <okennedy@buffalo.edu>
2023-12-15 15:17:50 -0500

    Fixing the bootstrap generator.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,5 +4,5 @@ coursier bootstrap $ARTIFACT\
   -r https://s01.oss.sonatype.org/content/groups/public/ \
   -r https://repo.osgeo.org/repository/release/ \
   -r https://maven.mimirdb.org/ \
-  -M info.vizierdb.Vizier
-  -o bin/vizier
+  -M info.vizierdb.Vizier \
+  -o bin/vizier -f
