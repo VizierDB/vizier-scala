b1725d43aa92b130f16c8364cb777f14ce7848d7
lyao4321 <lyao3@buffalo.edu>
2024-05-07 01:46:07 -0400

    removed barchartedit from menu since its not used and commented out completely

--- vizier/ui/src/info/vizierdb/ui/widgets/Menu.scala
+++ vizier/ui/src/info/vizierdb/ui/widgets/Menu.scala
@@ -4,7 +4,6 @@ import info.vizierdb.ui.components._
 import org.scalajs.dom
 import scalatags.JsDom.all._
 import info.vizierdb.ui.components.editors._
-import info.vizierdb.ui.components.editors.BarchartEditor
 
 
 
